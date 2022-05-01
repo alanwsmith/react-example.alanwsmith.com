e4d0bed3fea0b459997f2c93376c7c5c0c9696f1
Alan Smith <commit@alanwsmith.com>
2022-04-30 21:15:07 -0400

    Added home page example

--- site/src/App.js
+++ site/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg'
 import { Outlet, Link } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      <h1>React Router Example</h1>
       <Link to="/">Home</Link> <Link to="/examples">Examples</Link>
       <Outlet />
     </div>
